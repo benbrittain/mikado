8bd767dd8d0e1f03f79a84fbeaddc4cec3cbb801
Ben Brittain <ben@brittain.org>
2019-07-20 22:16:26 -0700

    Nicer error handling for the kernel

--- src/main.rs
+++ src/main.rs
@@ -18,8 +18,16 @@ fn panic(info: &PanicInfo) -> ! {
 
 #[no_mangle]
 pub extern "C" fn _rust_start() -> ! {
+    // Anything returned from main that isn't Ok is a system-wide panic
+    if let Err(error) = main() {
+        panic!("{}", error);
+    }
+    loop {}
+}
+
+pub fn main() -> Result<(), core::fmt::Error> {
     uart::initialize();
     println!("Coming back to where you started is not the same as never leaving.");
 
-    loop {}
+    Ok(())
 }
